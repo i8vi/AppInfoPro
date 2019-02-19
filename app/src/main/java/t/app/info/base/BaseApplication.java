e21f080a0cccf5aa3b4ac08f0bd9d85a80229750
Ttt <13798405957@163.com>
2019-02-19 16:43:33 +0800

    1.发布 1.3 版本 2.全面使用 DevUtils、DevQuickUtils 库实现该项目

--- app/src/main/java/t/app/info/base/BaseApplication.java
+++ app/src/main/java/t/app/info/base/BaseApplication.java
@@ -10,6 +10,7 @@ import dev.DevUtils;
 import dev.utils.app.logger.DevLogger;
 import dev.utils.app.logger.LogConfig;
 import dev.utils.app.logger.LogLevel;
+import t.app.info.BuildConfig;
 
 /**
  * detail: BaseApplication
@@ -25,18 +26,21 @@ public class BaseApplication extends Application {
         super.onCreate();
         // 初始化全局上下文
         DevUtils.init(getApplicationContext());
-        // == 初始化日志配置 ==
-        // 设置默认Logger配置
-        LogConfig logConfig = new LogConfig();
-        logConfig.logLevel = LogLevel.DEBUG;
-        logConfig.tag = LOG_TAG;
-        logConfig.displayThreadInfo = false;
-        logConfig.methodCount = 0;
-        logConfig.sortLog = true; // 美化日志, 边框包围
-        DevLogger.init(logConfig);
-        // 打开 lib 内部日志 - 线上环境, 不调用方法就行
-        DevUtils.openLog();
-        DevUtils.openDebug();
+        // 属于 DEBUG 才开启日志
+        if (BuildConfig.DEBUG){
+            // == 初始化日志配置 ==
+            // 设置默认Logger配置
+            LogConfig logConfig = new LogConfig();
+            logConfig.logLevel = LogLevel.DEBUG;
+            logConfig.tag = LOG_TAG;
+            logConfig.displayThreadInfo = false;
+            logConfig.methodCount = 0;
+            logConfig.sortLog = true; // 美化日志, 边框包围
+            DevLogger.init(logConfig);
+            // 打开 lib 内部日志 - 线上环境, 不调用方法就行
+            DevUtils.openLog();
+            DevUtils.openDebug();
+        }
 
         // android 7.0系统解决拍照的问题
         if (Build.VERSION.SDK_INT >=  Build.VERSION_CODES.JELLY_BEAN_MR2) {
